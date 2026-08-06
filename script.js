ac1159a7fe353ee815d8fe9f1045bf86d5f03cbe
meeranicholas <128350176+meeranicholas@users.noreply.github.com>
2026-08-06 17:19:33 +0400

    Auto mailing from contact form
    Auto mailing from contact form

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+console.log("Medcom script.js loaded");
 (function () {
     emailjs.init("Ymv-3RMFVNu5BkZaS");
 })();
@@ -13,6 +15,29 @@ document.getElementById("enquiry-form").addEventListener("submit", function(even
     .then(function() {
         alert("Thank you! Your enquiry has been submitted successfully.");
         document.getElementById("enquiry-form").reset();
+    }, function(error) {
+        alert("Failed to send enquiry. Please try again.");
+        console.log(error);
+    });
+});
+
+
+
+(function () {
+    emailjs.init("Ymv-3RMFVNu5BkZaS");
+})();
+
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    emailjs.sendForm(
+        "service_h1eh0bn",
+        "template_ilfhgxp",
+        this
+    )
+    .then(function() {
+        alert("Thank you for contacting Medom! Your enquiry has been submitted successfully.");
+        document.getElementById("contact-form").reset();
     }, function(error) {
         alert("Failed to send enquiry. Please try again.");
         console.log(error);
